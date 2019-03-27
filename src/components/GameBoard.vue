188abd47262a90ccb67b88b9611d4213264d115a
L16H7 <linmyatko@hotmail.com>
2019-03-27 17:01:24 +0630

    show ship

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -22,8 +22,11 @@
         v-btn.grid(
           block
           :disabled="!isGameReady"
+          @click="fireEnemyGrid( ((i - 1) * gridsPerRow) + j )"
         )
           span(v-if="!isPlayerReady") {{ ((i - 1) * gridsPerRow) + j }}
+          span(v-else)
+            v-icon {{ getIcon(  ((i - 1) * gridsPerRow) + j ) }}
 </template>
 <script>
 export default {
@@ -50,6 +53,10 @@ export default {
       return this.board.slice((index - 1) * this.gridsPerRow, index * this.gridsPerRow);
     },
 
+    getIcon(coordinate) {
+      return (coordinate === parseInt(this.position, 10)) ? 'directions_boat' : 'waves';
+    },
+
     setPlayerPosition() {
       if (this.position < 0 || this.position > 15) {
         this.position = 0;
@@ -57,6 +64,10 @@ export default {
       }
       this.$store.dispatch('setPlayerPosition', this.position);
       this.isPlayerReady = true;
+    },
+
+    fireEnemyGrid(coordinate) {
+      console.log(coordinate);
     },
   },
 };
